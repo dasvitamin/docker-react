d8d6b2271049913116b62134e1bda725694a0fe5
dasvitamin <dasvitamin@gmail.com>
2019-06-19 15:00:11 +0200

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Carlos 123456
+          Hello there
         </p>
         <a
           className="App-link"
